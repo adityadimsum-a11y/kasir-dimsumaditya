faa0a5ff4af738df99ef9ad1b198fff1bf03cabc
adityadimsum-a11y <adityadimsum@gmail.com>
2026-06-03 22:44:12 +0700

    Update TabDashboard.jsx

--- src/components/tabs/TabDashboard.jsx
+++ src/components/tabs/TabDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Calendar, Printer, Wallet, Coins, CreditCard, TrendingUp, ArrowRightLeft, Users, ShoppingCart, AlertCircle, Clock, Package, CheckCircle } from 'lucide-react';
-import { getTodayStr, formatRp, formatDate, generateId } from '../../utils/helpers';
+import { getTodayStr, getLocalYMD, formatRp, formatDate, generateId } from '../../utils/helpers';
 import SimpleSVGLineChart from '../ui/SimpleSVGLineChart';
 import useDashboardPusat from '../../hooks/useDashboardPusat';
 
