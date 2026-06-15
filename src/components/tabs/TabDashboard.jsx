5e1d0ad7feb615fd5095aaa4e1e19aa4222d663d
adityadimsum-a11y <adityadimsum@gmail.com>
2026-06-15 14:19:32 +0700

    Update TabDashboard.jsx

--- src/components/tabs/TabDashboard.jsx
+++ src/components/tabs/TabDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import { 
   TrendingUp, Wallet, Package, Users, AlertCircle, BarChart3, 
   ShieldCheck, Landmark, Globe, ArrowRightLeft, CreditCard,
-  Building2, Banknote, Activity
+  Building2, Banknote, Activity, Factory
 } from 'lucide-react';
 import { getTodayStr } from '../../utils/helpers';
 
