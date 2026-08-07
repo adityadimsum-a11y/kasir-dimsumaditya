ac95cc0cbdcb8b0728aa915c1dc0e17df6526048
adityadimsum-a11y <adityadimsum@gmail.com>
2026-08-07 20:02:03 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./styles/theme.css";
 import "./styles/globals.css";
 import "./styles/professional.css";
+import "./styles/operations-v2.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
