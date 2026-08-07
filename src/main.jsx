3164ca363a749413cb93098980e12b3f536c98e9
adityadimsum-a11y <adityadimsum@gmail.com>
2026-08-07 22:41:08 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./styles/theme.css";
 import "./styles/globals.css";
 import "./styles/professional.css";
 import "./styles/operations-v2.css";
+import "./styles/workspace-v3.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
